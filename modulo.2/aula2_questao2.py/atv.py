16399419e1046d7d40a7bc0146940447ce03aad7
Gabriel Marçal <gabrielbd70@gmail.com>
2025-07-23 10:01:36 -0300

    Add files via upload

---
+++ modulo.2/aula2_questao2.py/atv.py
@@ -0,0 +1,12 @@
+resultado ="o resultado é"
+variavel_2 = 10
+variavel_3 = 3.5
+variavel_4 = variavel_2 + variavel_3
+print(resultado)
+print(type(resultado))
+print(variavel_2)
+print(type(variavel_2))
+print(variavel_3)
+print(type(variavel_3))
+print(variavel_4)
+print(type(variavel_4))
