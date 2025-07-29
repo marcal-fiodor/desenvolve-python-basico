4244c82fc3990334cbcfa8e3c05eae7b6a11ca69
Gabriel Marçal <gabrielbd70@gmail.com>
2025-07-29 14:23:02 -0300

    Add files via upload

---
+++ modulo.3/aula4_questao2.py/atv.py
@@ -0,0 +1,25 @@
+#Entrada de dados
+nota = int(input("Avalie o filme com uma nota de 1 a 5: "))
+
+#Processamento
+av_1 = "Ruim."
+av_2 = "Regular."
+av_3 = "Bom!"
+av_4 = "Muito Bom!"
+av_5 = "Excelente!"
+
+#Saída de dados
+if nota == 1:
+    print("O filme é", av_1)
+
+if nota == 2:
+    print("O filme é", av_2)
+
+if nota == 3:
+    print("O filme é", av_3)
+
+if nota == 4:
+    print("O filme é", av_4)
+
+if nota == 5:
+    print("O filme é", av_5)
