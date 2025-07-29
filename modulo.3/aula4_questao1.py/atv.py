32f331536a980a12cde48a9d6c2c87839e67d2d0
Gabriel Marçal <gabrielbd70@gmail.com>
2025-07-29 14:07:54 -0300

    Add files via upload

---
+++ modulo.3/aula4_questao1.py/atv.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+#Processamento
+soma = n1 + n2
+#Saída de dados
+if soma % 2 == 0:
+    print("A soma dos números é par")
+else:
+    print("A soma dos números é impar")
